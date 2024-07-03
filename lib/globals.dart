df47c3606429184d22d273206582e35e454c795b
lekiet524 <lekiet524@gmail.com>
2024-07-03 09:53:50 +0700

    delete
    delete project

--- lib/globals.dart
+++
@@ -1,5 +0,0 @@
-// TODO Implement this library.// lib/globals.dart
-import 'package:one_store/data/model/product_model.dart';
-
-List<ProductModel> favoriteProducts = [];
-List<ProductModel> allProducts = [];
